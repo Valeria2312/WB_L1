fb93cfe2a68f875ea9717d58dfc424306cd8409a
Valeria <Koroleva.Valeria.97@yandex.ru>
2023-09-04 19:01:28 +0300

    задача 18

---
+++ 18_DataVolume_LocalStorage/18_DataVolume_LocalStorage_2.js
@@ -0,0 +1,20 @@
+
+function changeLocalStorage() {
+    //провяем поддерживает ли браузер localStorage и не была ли уже определена максимальная емкость данных
+    if (localStorage && !localStorage.getItem('size')) {
+        //используем для увеличения размера данных
+        let i = 0;
+        //Если при выполнении кода возникает исключение, то выполнение переходит в блок catch, где будет обработано это исключение.
+        try {
+            for (i = 250; i <= 10000; i += 250) {
+                localStorage.setItem('test', new Array((i * 1024) + 1).join('a'));
+            }
+        } catch (e) {
+            localStorage.removeItem('test');
+            //убираем то что уже не поместилось
+            localStorage.setItem('size', i - 250);
+        }
+    }
+
+}
+changeLocalStorage()
